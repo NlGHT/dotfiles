f385508e162e280998f36e2db7891034b214456d
NlGHT <tnight17@student.aau.dk>
2020-07-06 16:45:50 +0200

    renameAllHere is fully functional and 1.0

--- .bin/BashCScripts/Files/renameAllHere.cpp
+++ .bin/BashCScripts/Files/renameAllHere.cpp
@@ -1,6 +1,6 @@
 /*
  * Make sure to compile with options -std=c++17 and -lstdc++fs.
- * For example: g++ -std=c++17 -lstdc++fs renameAllHere.cpp -o renameAllHere
+ * For example: g++ renameAllHere.cpp -std=c++17 -o renameAllHere -lstdc++fs
  * Or for debugging with gdb: g++ -g -std=c++17 -lstdc++fs renameAllHere.cpp -o renameAllHere
  *
  */
@@ -12,7 +12,6 @@
 #include <experimental/filesystem>
 #include <system_error>
 #include <vector>
-#include <deque>
 namespace fs = std::experimental::filesystem;
 
 struct fileRename {
@@ -22,149 +21,37 @@ struct fileRename {
         : originalName(origName), newName(inNewName) {}
 };
 
-
-/*
- * DEBUGGING
- */
-// void printOrdering(std::deque<std::unique_ptr<fileRename>>& filesToRename) {
-    // for (auto& rnObject : filesToRename) {
-        // std::cout << "Old name: " << rnObject->originalName << ".  New name: " << rnObject->newName << std::endl;
-    // }
-// }
-
-// void correctIteratorToName(std::deque<std::unique_ptr<fileRename>>::iterator& it, std::string otherNewName,
-        // std::deque<std::unique_ptr<fileRename>>& filesToRename) {
-    // it -= 1;
-    // if (otherNewName.compare((*it)->newName) != 0) {
-        // for (auto it = filesToRename.begin(); it != filesToRename.end(); ++it) {
-            // if (otherNewName.compare((*it)->newName) == 0) {
-                // break;
-            // }
-        // }
-    // }
-// }
-
-// void printDescrepency(std::deque<std::unique_ptr<fileRename>>& filesToRename) {
-    // std::vector<std::string> newNames;
-    // for (auto& thing : filesToRename) {
-        // // std::cout << newNames.size() << std::endl;
-        // if (newNames.size() == 0)
-            // newNames.push_back(thing->newName);
-        // else {
-            // for (std::string name : newNames) {
-                // if (name.compare(thing->originalName) == 0) {
-                    // std::cout << "Descrepency: " << thing->originalName << std::endl;
-                    // break;
-                // }
-            // }
-
-            // newNames.push_back(thing->newName);
-        // }
-    // }
-// }
-/*
- * END Debugging
- */
-
-
-/*
- * START Main Content
- */
 bool bitHas(const unsigned int &bitArgs, const unsigned int position) {
     return ((bitArgs & (1 << position)) != 0);
 }
 
 void addBit(unsigned int &bitArgs, const unsigned int position) {
-    bitArgs |= (1 << position); // sets the 5th-from right
-}
-
-void renameFile(const char* origName, const std::string outName, int bitArgs) {
-	int result = rename(origName, outName.c_str());
-	if (result == 0) {
-        if (!bitHas(bitArgs, 1)) {
-            // If there is no --silent passed in
-            std::cout << "File: " << origName << " renamed to: " << outName << std::endl;
-        }
-    } else {
-		perror("Error renaming file");
-    }
+    bitArgs |= (1 << position);
 }
 
 void renameFile(std::unique_ptr<fileRename>& rnObject, int bitArgs) {
     std::error_code ec;
-    if (bitHas(bitArgs, 3)) {
-        // Then the --safest option is given
-        if (fs::exists(rnObject->newName)) {
-            ec.default_error_condition();
-            if (!bitHas(bitArgs, 1) && !bitHas(bitArgs, 15))
-                std::cout << "File for some reason already existed: " << rnObject->newName << std::endl;
-        } else {
-            fs::rename(rnObject->originalName, rnObject->newName, ec);
-        }
+    if (fs::exists(rnObject->newName)) {
+        ec.default_error_condition();
+        if (!bitHas(bitArgs, 1) && !bitHas(bitArgs, 15))
+            // If there is no --silent passed in or the rename count is over 500
+            std::cout << "File: " << rnObject->newName << " already exists, skipping..." << std::endl;
     } else {
-        // Just rename without necessarily checking
         fs::rename(rnObject->originalName, rnObject->newName, ec);
-    }
-
-    if (!bitHas(bitArgs, 1) && !bitHas(bitArgs, 15)) {
-        // If there is no --silent passed in
-        std::cout << "File: " << rnObject->originalName << " renamed to: " << rnObject->newName << std::endl;
+        if (!bitHas(bitArgs, 1) && !bitHas(bitArgs, 15))
+            // If there is no --silent passed in or the rename count is over 500
+            std::cout << "File: " << rnObject->originalName << " renamed to: " << rnObject->newName << std::endl;
     }
 }
 
-bool addFileAndDeleteIfThere(std::string oldName, std::string newName,
-        std::deque<std::unique_ptr<fileRename>>& filesToRename, const unsigned int &bitArgs) {
-    bool isCorrected = false;
-    int positionLoopSeen = -1;
-    int loopCounter = 0;
-    for (auto it = filesToRename.begin(); it != filesToRename.end(); it++) {
-        if ((*it)->originalName.compare((*it)->newName) == 0) {
-            // If the old name of the file is going to be renamed to the new one
-            it = filesToRename.erase(it) - 1;
-        } else {
-            if (oldName.compare((*it)->newName) == 0) {
-                if (newName.compare((*it)->originalName) == 0) {
-                    // If the edited new name being set is equal to the object's old name, then there is nothing that needs renaming
-                    it = filesToRename.erase(it) - 1;
-                } else {
-                    if (positionLoopSeen > 0) {
-                        filesToRename[positionLoopSeen]->originalName.assign((*it)->originalName);
-                        it = filesToRename.erase(it) - 1;
-                    } else {
-                        (*it)->newName.assign(newName);
-                        positionLoopSeen = loopCounter;
-                    }
-                }
-
-                isCorrected = true;
-            } else if (newName.compare((*it)->originalName) == 0) {
-                if (positionLoopSeen > 0) {
-                    filesToRename[positionLoopSeen]->newName.assign((*it)->newName);
-                    it = filesToRename.erase(it) - 1;
-                } else {
-                    (*it)->originalName.assign(oldName);
-                    positionLoopSeen = loopCounter;
-                }
-
-                isCorrected = true;
-            }
-        }
-
-        loopCounter++;
-    }
-
-    if (isCorrected) {
-        return true;
-    } else {
-        std::unique_ptr<fileRename> fileToBeRenamed(new fileRename(oldName, newName));
-        filesToRename.push_back(std::move(fileToBeRenamed));
-    }
-    return false;
+void addFileRename(std::string oldName, std::string newName, std::vector<std::unique_ptr<fileRename>>& filesToRename) {
+    std::unique_ptr<fileRename> fileToBeRenamed(new fileRename(oldName, newName));
+    filesToRename.push_back(std::move(fileToBeRenamed));
 }
 
 int renameInDir(const char* path, const std::string stringInput, unsigned int bitArgs) {
     unsigned int i = 0;
-    std::deque<std::unique_ptr<fileRename>> filesToRename;
+    std::vector<std::unique_ptr<fileRename>> filesToRename;
 
     if (!bitHas(bitArgs, 1))
         std::cout << "Building and correcting database of files to be renamed..." << std::endl;
@@ -183,7 +70,7 @@ int renameInDir(const char* path, const std::string stringInput, unsigned int bi
             buildName += extension;
         }
 
-        bool result = addFileAndDeleteIfThere(path.filename().string(), buildName, filesToRename, bitArgs);
+        addFileRename(path.filename().string(), buildName, filesToRename);
     }
 
     if (!bitHas(bitArgs, 1)) {
@@ -203,11 +90,6 @@ int renameInDir(const char* path, const std::string stringInput, unsigned int bi
     if (!bitHas(bitArgs, 1)) {
         std::cout << "Successfully renamed " << filesToRename.size() << " files." << std::endl;
     }
-
-    // std::cout << "Final print out" << std::endl;
-    // printOrdering(filesToRename);
-    // std::cout << "Descrepencies:" << std::endl;
-    // printDescrepency(filesToRename);
 
     return 0;
 }
@@ -246,8 +128,6 @@ unsigned int checkArgs(int* argCount, char* argv[]) {
             if (std::strncmp("--silent", argv[i], 8) == 0) {
                 // Then silent arg is active
                 addBit(bitArgs, 1);
-            } else if (std::strncmp("--safest", argv[i], 8) == 0) {
-                addBit(bitArgs, 3);
             }
 
             countOptionArgs++;
@@ -268,7 +148,6 @@ int main(int argc, char* argv[]) {
     /*
      * OPTIONS:
      * --silent : No printing of files renamed.
-     * --safest : File renaming will check if the file exists first (I can't see a case where it would, but just in case)
      *
      */
 
